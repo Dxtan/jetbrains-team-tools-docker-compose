1754a8e56d5e8d7c0b42438f5abd38c914a1327e
tandexiang <tandexiang@shein.com>
2023-10-13 14:24:49 +0800

    update

--- cleanup.sh
+++ cleanup.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 rm -rf ./buildserver_pgdata/*
-rm -rf ./teamcity/datadir/config/*
+rm -rf ./teamcity/datadir/config/*.dist
 rm -rf ./teamcity/datadir/system/*
 rm -rf ./teamcity/teamcity-server-logs/*
+
+rm -rf ./hub/conf/*
+rm -rf ./hub/logs/*
+
+rm -rf ./youtrack/conf/*
+rm -rf ./youtrack/logs/*
